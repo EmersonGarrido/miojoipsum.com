301b5e75556ee5d257d2b179e2f10aca1d521418
EmersonGarrido <emersongarrido@hotmail.com.br>
2022-07-08 19:22:16 -0400

    chore:

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,24 @@ import { miojoQuotes } from 'lib/quotes'
 
 const Home: NextPage = () => {
   const [lorem, setLorem] = useState("");
-  const [paragarph, setParagarph] = useState(0);
+
+
+  function handleGerateText() {
+    const isLorem = loremIpsum({
+      count: 1,                // Number of "words", "sentences", or "paragraphs"
+      format: "plain",         // "plain" or "html"
+      paragraphLowerBound: 1,  // Min. number of sentences per paragraph.
+      paragraphUpperBound: 1,  // Max. number of sentences per paragarph.
+      random: Math.random,     // A PRNG function
+      // sentenceLowerBound: 1,   // Min. number of words per sentence.
+      // sentenceUpperBound: paragarph,  // Max. number of words per sentence.
+      suffix: "\n",            // Line ending, defaults to "\n" or "\r\n" (win32)
+      units: "paragraph",      // paragraph(s), "sentence(s)", or "word(s)"
+      words: miojoQuotes      // Array of words to draw from
+    });
+    setLorem(isLorem)
+  }
+
   useEffect(() => {
     const isLorem = loremIpsum({
       count: 1,                // Number of "words", "sentences", or "paragraphs"
@@ -33,8 +50,8 @@ const Home: NextPage = () => {
       <main className="flex flex-col gap-4 justify-center items-center md:mb-[200px] md:mt-[150px]">
         <img src="/logo.png" />
 
-        <h1 className="text-[#415160] text-2xl font-bold mb-5">Quantos parágrafos de miojo você precisa?</h1>
-
+        <div className="flex flex-col justify-center items-center"><h1 className="text-[#415160] text-center text-2xl font-bold mb-5">Precisa de um texto mais rápido que um miojo?</h1>
+          <button onClick={() => handleGerateText()} className="bg-[#e57373] hover:bg-[#e57373]/80 border-0 p-2 pl-4 pr-4 text-2xl rounded-md text-white">Gerar novo texto</button></div>
         <div className="flex flex-col md:flex-row justify-center items-center gap-3">
           <div className="md:w-[500px] w-full md:h-[500px] bg-white p-4">
             <h2 className="text-[#5274B4] text-4xl font-bold">Melhore seus trabalhos com a redaçāo nota 560 do Enem.</h2>
