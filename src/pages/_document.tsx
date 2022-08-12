2a2b71ca970bc6fd5c6f2f20c46623253f970e32
Emerson Garrido <emersongarrido@hotmail.com.br>
2022-08-11 20:28:07 -0400

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,4 @@
-import Document, {
-  Html,
-  Head,
-  Main,
-  NextScript,
-  DocumentContext,
-} from "next/document";
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
     const initialProps = await Document.getInitialProps(ctx);
@@ -13,29 +7,33 @@ class MyDocument extends Document {
 
   render() {
     return (
-      <Html lang="pt-BR">
+      <Html lang='pt-BR'>
         <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel='preconnect' href='https://fonts.googleapis.com' />
           <link
-            href="https://fonts.googleapis.com/css2?family=Edu+QLD+Beginner:wght@400;500&display=swap"
-            rel="stylesheet"
+            href='https://fonts.googleapis.com/css2?family=Edu+QLD+Beginner:wght@400;500&display=swap'
+            rel='stylesheet'
           />
           <script
             async
-            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5309395025758950"
+            src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5309395025758950'
           ></script>
-         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6176815543309144"
-     crossorigin="anonymous"></script>
-      <!-- miojoipsum-responsivo -->
-      <ins class="adsbygoogle"
-           style="display:block"
-           data-ad-client="ca-pub-6176815543309144"
-           data-ad-slot="9927376506"
-           data-ad-format="auto"
-           data-full-width-responsive="true"></ins>
-      <script>
-           (adsbygoogle = window.adsbygoogle || []).push({});
-      </script>
+          <script
+            async
+            src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6176815543309144'
+            crossOrigin='anonymous'
+          ></script>
+          <ins
+            className='adsbygoogle'
+            style={{
+              display: 'block',
+            }}
+            data-ad-client='ca-pub-6176815543309144'
+            data-ad-slot='9927376506'
+            data-ad-format='auto'
+            data-full-width-responsive='true'
+          ></ins>
+          <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
         </Head>
         <body>
           <Main />
