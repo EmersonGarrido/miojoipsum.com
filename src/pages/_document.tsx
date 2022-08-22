fc9cd337e2777fa47fafef64d18de1d52b540026
Emerson Garrido <emersongarrido@hotmail.com.br>
2022-08-21 23:04:18 -0400

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -14,26 +14,8 @@ class MyDocument extends Document {
             href='https://fonts.googleapis.com/css2?family=Edu+QLD+Beginner:wght@400;500&display=swap'
             rel='stylesheet'
           />
-          <script
-            async
-            src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5309395025758950'
-          ></script>
-          <script
-            async
-            src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6176815543309144'
-            crossOrigin='anonymous'
-          ></script>
-          <ins
-            className='adsbygoogle'
-            style={{
-              display: 'block',
-            }}
-            data-ad-client='ca-pub-6176815543309144'
-            data-ad-slot='9927376506'
-            data-ad-format='auto'
-            data-full-width-responsive='true'
-          ></ins>
-          <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
+          <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5309395025758950"
+     crossorigin="anonymous"></script>
         </Head>
         <body>
           <Main />
